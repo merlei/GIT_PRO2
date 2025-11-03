d11f15971aadb970c87859d4e17a53016e9980b4
Merlei <549683789@qq.com>
2025-11-03 18:35:01 +0800

    编写main.pysoftware

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# @Time : 2025/11/3 10:00
+# @Author : hly
+# @File : main.py
+# @Software: PyCharm
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()      
